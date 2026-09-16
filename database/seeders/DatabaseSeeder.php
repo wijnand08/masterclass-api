a6a39cf58d7b4c682aef166404317873d2353c44
wijnand08 <53388@hoornbeeck.nl>
2026-09-16 11:43:03 +0200

    3e video

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $users=\App\Models\User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        \App\Models\Ticket::factory(100)
+            ->recycle($users)
+            ->create();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Models\Ticket;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -11,3 +14,7 @@ Route::get('/user', function (Request $request) {
 
 Route::post('/login', [Authcontroller::class, 'login']);
 Route::post('/register', [Authcontroller::class, 'register']);
+
+Route::get('/tickets', function() {
+    return Ticket::all();
+});
